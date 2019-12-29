93b7eff4f0f6fc85cd723ab154bc3b94100eca43
Shane Logsdon <shane@shanelogsdon.com>
2019-12-29 13:15:29 -0500

    handle more events from crossterm

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,68 @@ extern crate crossterm;
 extern crate futures;
 
 use crossterm::{
-    event::{Event, EventStream, KeyCode},
+    event::{Event, EventStream, KeyCode, KeyEvent, MouseEvent},
     Result,
     terminal::{disable_raw_mode, enable_raw_mode}
 };
 use futures::{future::FutureExt, select, StreamExt};
 
-async fn accept_keypress(reader: &mut EventStream) {
+#[derive(Clone, Copy, Debug)]
+enum EditorEvent {
+    NoOp,
+    Exit,
+    KeyPress(KeyEvent),
+    Mouse(MouseEvent),
+    Resize(u16, u16),
+}
+
+async fn accept_editor_event(reader: &mut EventStream) -> Result<EditorEvent> {
     let mut event = reader.next().fuse();
 
     select! {
         maybe_event = event => {
             match maybe_event {
                 Some(Ok(event)) => {
-                    println!("Event::{:?}\r", event);
+                    match event {
+                        Event::Key(e) => {
+                            let event: KeyEvent = e;
 
-                    if event == Event::Key(KeyCode::Char('c').into()) {
-                        println!("hi c");
-                    }
+                            if event.code == KeyCode::Esc {
+                                return Ok(EditorEvent::Exit);
+                            }
 
-                    if event == Event::Key(KeyCode::Esc.into()) {
-                        break;
+                            Ok(EditorEvent::KeyPress(event))
+                        },
+                        Event::Mouse(e) => Ok(EditorEvent::Mouse(e)),
+                        Event::Resize(x, y) => Ok(EditorEvent::Resize(x, y)),
                     }
                 }
-                Some(Err(e)) => println!("Error: {:?}\r", e),
-                None => break,
+                Some(Err(e)) => Err(e),
+                None => Ok(EditorEvent::NoOp),
             }
         }
     }
 }
 
+fn hande_editor_event(_event: EditorEvent) {}
+
 async fn main_loop() {
     let mut reader = EventStream::new();
-    let mut buffer = Vec::<Event>::new();
+    let mut history = Vec::<EditorEvent>::new();
 
     loop {
-        accept_keypress(&mut reader).await;
+        let event = accept_editor_event(&mut reader).await;
+
+        match event {
+            Err(e) => panic!("Error: {}", e),
+            Ok(EditorEvent::NoOp) => continue,
+            Ok(EditorEvent::Exit) => break,
+            Ok(e) => {
+                println!("Event: {:?}", e);
+                history.push(e);
+                hande_editor_event(e);
+            },
+        }
     }
 }
 
