93f6916305239c9421817a6669c1ac7a41b0bb50
Shane Logsdon <shane@shanelogsdon.com>
2019-12-29 19:09:27 -0500

    remove panic

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -20,11 +20,12 @@ use editor::{
 
 fn main() -> Result<()> {
     terminal::setup_editor()?;
-    async_std::task::block_on(main_loop());
+    async_std::task::block_on(main_loop())?;
     terminal::teardown_editor()
 }
 
-async fn main_loop() {
+async fn main_loop() -> Result<()> {
+    let mut error: Option<crossterm::ErrorKind> = None;
     let mut reader = EventStream::new();
     let mut state = WindowState::new();
 
@@ -32,7 +33,10 @@ async fn main_loop() {
         let event = accept_window_input(&mut reader).await;
 
         match event {
-            Err(e) => panic!("Error: {}", e),
+            Err(e) => {
+                error = Some(e);
+                break;
+            },
             Ok(WindowInputEvent::NoOp) => continue,
             Ok(WindowInputEvent::Exit) => break,
             Ok(e) => {
@@ -42,4 +46,10 @@ async fn main_loop() {
             },
         }
     }
+
+    if let Some(e) = error {
+        return Err(e);
+    }
+
+    Ok(())
 }
