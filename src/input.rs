04d24fe5ea7d785ecc72ad1003fe10ca2f036cd3
Shane Logsdon <shane@shanelogsdon.com>
2019-12-29 20:09:48 -0500

    refactor crossterm event conversion

--- src/input.rs
+++ src/input.rs
@@ -4,7 +4,7 @@ use crossterm::{
 };
 use futures::{future::FutureExt, select, StreamExt};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 pub enum WindowInputEvent {
     NoOp,
     Exit,
@@ -13,13 +13,32 @@ pub enum WindowInputEvent {
     Resize(u16, u16),
 }
 
+impl WindowInputEvent {
+    pub fn from_crossterm_event(event: Event) -> WindowInputEvent {
+        match event {
+            Event::Key(e) => {
+                let event: KeyEvent = e;
+
+                // TODO: remove with command mode
+                if event.code == KeyCode::Esc {
+                    return WindowInputEvent::Exit;
+                }
+
+                WindowInputEvent::KeyPress(event)
+            },
+            Event::Mouse(e) => WindowInputEvent::Mouse(e),
+            Event::Resize(x, y) => WindowInputEvent::Resize(x, y),
+        }
+    }
+}
+
 pub async fn accept_window_input(reader: &mut EventStream) -> Result<WindowInputEvent> {
     let mut event = reader.next().fuse();
 
     select! {
         maybe_event = event => {
             match maybe_event {
-                Some(Ok(e)) => convert_crossterm_event_to_window_input_event(e),
+                Some(Ok(e)) => Ok(WindowInputEvent::from_crossterm_event(e)),
                 Some(Err(e)) => Err(e),
                 None => Ok(WindowInputEvent::NoOp),
             }
@@ -28,20 +47,3 @@ pub async fn accept_window_input(reader: &mut EventStream) -> Result<WindowInput
 }
 
 pub fn handle_window_input(_event: WindowInputEvent) {}
-
-fn convert_crossterm_event_to_window_input_event(event: Event) -> Result<WindowInputEvent> {
-    match event {
-        Event::Key(e) => {
-            let event: KeyEvent = e;
-
-            // TODO: remove with command mode
-            if event.code == KeyCode::Esc {
-                return Ok(WindowInputEvent::Exit);
-            }
-
-            Ok(WindowInputEvent::KeyPress(event))
-        },
-        Event::Mouse(e) => Ok(WindowInputEvent::Mouse(e)),
-        Event::Resize(x, y) => Ok(WindowInputEvent::Resize(x, y)),
-    }
-}
